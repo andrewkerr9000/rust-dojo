ef175ac7c02ee97389cc93d26c55dd0438aaf380
andrew kerr <andrewkerr9000@gmail.com>
2014-08-06 17:18:25 +0100

    Revert "merging in cleanup from other branch"
    This reverts commit 6e706c62b6a56c716b1fa62e00c365b60fa6d41a.
    
    Remove answers to dojo1

--- src/syntax/mod.rs
+++ src/syntax/mod.rs
@@ -1,64 +1,41 @@
 mod tests;
 
 fn compare(a: int, b:int) -> bool {
-  a>b
+  true
 }
 
 fn compare_int(a: int, b: int) -> int {
-  if a>b {
-    1
-  } else {
-    0
-  }
+  3
 }
 
 fn compare_pair(pair:(int,int)) -> bool {
-  let (a,b) = pair;
-  compare(a,b)
+  true
 }
 
 fn contains_zero(pair:(int,int)) -> bool {
-  match pair {
-    (0,_) => true,
-    (_,0) => true,
-    _     => false
-  }
+  true
 }
 
 fn first_non_zero(pair:(int,int)) -> int {
-  match pair {
-    (0,a) => a,
-    (a,0) => a,
-    _     => 0
-  }
+  7
 }
 
 fn compare_order(pair:(int,int)) -> int {
-  match pair {
-    (a,b) if a>b  => 1,
-    (a,b) if a<b  => -1,
-    (a,b) if a==b => 0,
-    _             => 0
-  }
+  94
 }
 
 fn fib(n: int) -> int {
-  match n {
-    0 => 0,
-    1 => 1,
-    m => fib(m-1) + fib(m-2)
-  }
+  -100
 }
 
 fn div(dividend: int, divisor: int) -> Option<int> {
-  if divisor == 0 { None }
-  else { Some(dividend / divisor) }
+  None
 }
 
 fn get_or_minus_one(val: Option<int>) -> int {
-  val.unwrap_or(-1)
+  999
 }
 
 fn double(val: Option<int>) -> Option<int> {
-  val.map(|x| x * 2)
+  None
 }
